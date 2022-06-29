875c89d86f4fab7ca8fb5339b108f3874b947827
JavierRibaldelRio <javierribal@gmail.com>
2022-06-29 11:41:23 +0200

    He avanzado con el primer proyecto

--- Front-End-Development-Libraries/random-quote-machine/src/App.js
+++ Front-End-Development-Libraries/random-quote-machine/src/App.js
@@ -1,13 +1,58 @@
 import './App.css';
-import bootstrap from 'bootstrap'
 import QuoteBox from './components/QuoteBox';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App container-fluid">
-      <QuoteBox />
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props)
+      ; this.state = {
+        colores: [
+          '#663F46', '#00Bb2D', '#e8C138', '#FE5E41', '#344966', '#9D8420',
+          '#E9aaa9', '#355834', '#DF367C', '#FCA17D', '#0F5257', '#CC5803',
+          '#BCD1BA', '#81F495', '#878E88'
+        ],
+
+        edad: 88
+
+      }
+
+    this.numeroAleatorio = this.numeroAleatorio.bind(this);
+    this.nuevoColor = this.nuevoColor.bind(this);
+
+  }
+
+  numeroAleatorio(maxim) {
+    return Math.floor(Math.random() * maxim);
+  }
+
+  nuevoColor() {
+
+    this.setState({ color: this.state.colores[this.numeroAleatorio(this.state.colores.length)] });
+  }
+
+  componentDidMount() {
+
+    this.nuevoColor();
+  }
+
+  render() {
+
+    const estilo = {
+      backgroundColor: this.state.color,
+      transition: "all ease",
+
+      transitionDuration: '1.75s',
+    }
+
+
+
+    return (
+      <div className="App container-fluid" style={estilo} >
+        <QuoteBox color={this.state.color} cambioColor={this.nuevoColor} numeroAleatorio={this.numeroAleatorio} />
+      </div>
+    );
+  }
 }
 
 export default App;
--- Front-End-Development-Libraries/random-quote-machine/src/components/QuoteBox.js
+++ Front-End-Development-Libraries/random-quote-machine/src/components/QuoteBox.js
@@ -5,47 +5,56 @@ class QuoteBox extends Component {
 
     constructor(props) {
         super(props);
-        this.state = { quote: undefined }
+        this.state = {
+            quote: undefined,
+        }
 
         this.nuevaCuota = this.nuevaCuota.bind(this);
+        this.reproducirSonido = this.reproducirSonido.bind(this);
 
-    }
-
-
-    numeroAleatorio(maxim) {
-        return Math.floor(Math.random() * maxim);
     }
 
     nuevaCuota() {
 
         const numeroMaximo = this.state.quotes.length
 
-        this.setState({ quote: this.state.quotes[this.numeroAleatorio(numeroMaximo)] });
+        this.props.cambioColor();
+
+        this.setState({ quote: this.state.quotes[this.props.numeroAleatorio(numeroMaximo)] });
 
     }
-
 
     componentDidMount() {
 
         fetch("https://type.fit/api/quotes")
             .then(response => response.json())
             .then(data => {
-
-                this.setState({ quotes: data, quote: data[this.numeroAleatorio(data.length)] });
-
+                this.setState({ quotes: data, quote: data[this.props.numeroAleatorio(data.length)] });
             });
+
+    }
+
+    reproducirSonido() {
+
+        var audio = new Audio("tweet.mp3");
+
+        audio.play();
     }
 
 
     render() {
 
+
         if (this.state.quote === undefined) {
             return (<></>)
         }
         else {
+
+
+
             const autor = (this.state.quote.author === null) ? "Anonimus" : "By " + this.state.quote.author;
 
-            return (<div id="quote-box" className='container-fluid'>
+            return (<div id="quote-box" className='container-fluid' >
 
 
                 <div id="text">
@@ -55,23 +64,19 @@ class QuoteBox extends Component {
 
                 <div id='parte-derecha'>
 
-
-
-                    <a id="tweet-quote" target="_top" href="www.twitter.com/intent/tweet">
-                        <i class="fa-brands fa-twitter-square"></i>
+                    <a id="tweet-quote" target="_blank" rel="noreferrer" href="https://twitter.com/intent/tweet" onMouseEnter={this.reproducirSonido}>
+                        <i className="fa-brands fa-twitter-square"></i>
                     </a>
 
                     <div id="author">
                         {autor}
                     </div>
 
-                    <button id="new-quote" className="btn btn-primary" onClick={this.nuevaCuota}>
+                    <button id="new-quote" className="btn btn-outline-light" onClick={this.nuevaCuota}>
 
-                        <i class="fa-solid fa-arrows-rotate"></i> Change Quote
+                        <i className="fa-solid fa-arrows-rotate"></i> Change Quote
 
                     </button>
-
-
                 </div>
             </div>);
         }
