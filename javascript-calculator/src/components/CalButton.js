ee5fd1f7523f8f91601586e14638a95c65d3c13d
JavierRibaldelRio <javierribal@gmail.com>
2022-07-26 10:32:47 +0200

    He creado el boton para la calculadora

---
+++ javascript-calculator/src/components/CalButton.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+
+class Boton extends Component {
+    constructor(props) {
+        super(props);
+    }
+    state = {}
+    render() {
+        return (  );
+    }
+}
+
+Boton.defaultProps = {
+
+    contenido: ''
+}
+
+export default Boton;
