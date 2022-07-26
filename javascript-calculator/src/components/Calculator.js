a447f9211c150f0a64ddfc568c2e1e462214bb0d
JavierRibaldelRio <javierribal@gmail.com>
2022-07-26 10:33:18 +0200

    He creado la calculadora

---
+++ javascript-calculator/src/components/Calculator.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class Calculator extends Component {
+    constructor(props) {
+        super(props);
+    }
+    state = {}
+
+
+    render() {
+        return (<div id="calculator">
+
+
+
+        </div>);
+    }
+}
+
+export default Calculator;
